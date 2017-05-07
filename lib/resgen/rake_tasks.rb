543c6617957657ed0f0bcc69b856e239c37713cc
Peter Donald <peter@realityforge.org>
2017-05-08 03:24:17 +1000

    Update to use the latest reality-generators gem

--- lib/resgen/rake_tasks.rb
+++ lib/resgen/rake_tasks.rb
@@ -57,163 +57,56 @@ module Resgen #nodoc
     end
   end
 
-  class GenerateTask
-    attr_accessor :description
-    attr_accessor :namespace_key
-    attr_accessor :filter
-    attr_writer :verbose
-
-    attr_reader :repository_key
-    attr_reader :key
-    attr_reader :generator_keys
-    attr_reader :target_dir
-
-    attr_reader :task_name
-
+  class GenerateTask < Reality::Generators::Rake::BaseGenerateTask
     def initialize(repository_key, key, generator_keys, target_dir, buildr_project = nil)
-      @repository_key, @key, @generator_keys, @buildr_project =
-        repository_key, key, generator_keys, buildr_project
-      @namespace_key = :resgen
-      @filter = nil
-      @template_map = {}
-      # Turn on verbose messages if buildr is turned on tracing
-      @verbose = trace?
-      @target_dir = target_dir
-      yield self if block_given?
-      define
-      @templates = Resgen::Generator.generator.load_templates_from_template_sets(generator_keys)
-      Reality::Generators::Buildr.configure_buildr_project(buildr_project, task_name, @templates, target_dir)
+      super(repository_key, key, generator_keys, target_dir, buildr_project)
     end
 
-    private
+    protected
 
-    def verbose?
-      !!@verbose
+    def default_namespace_key
+      :resgen
     end
 
-    def full_task_name
-      "#{self.namespace_key}:#{self.key}"
+    def generator_container
+      Resgen::Generator
     end
 
-    def define
-      desc self.description || "Generates the #{key} artifacts."
-      namespace self.namespace_key do
-        t = task self.key => ["#{self.namespace_key}:load"] do
-          begin
+    def instance_container
+      Resgen
+    end
 
-            repository = nil
-            if self.repository_key
-              repository = Resgen.repository_by_name(self.repository_key)
-              if Resgen.repositories.size == 1
-                Resgen.warn("Resgen task #{full_task_name} specifies a repository_key parameter but it can be be derived as there is only a single repository. The parameter should be removed.")
-              end
-            elsif self.repository_key.nil?
-              repositories = Resgen.repositories
-              if repositories.size == 1
-                repository = repositories[0]
-              else
-                Resgen.error("Resgen task #{full_task_name} does not specify a repository_key parameter and it can not be derived. Candidate repositories include #{repositories.collect { |r| r.name }.inspect}")
-              end
-            end
+    def validate_root_element(element)
+      element.send(:extension_point, :scan_if_required)
+      element.send(:extension_point, :validate)
+    end
 
-            repository.send(:extension_point, :scan_if_required)
-            repository.send(:extension_point, :validate)
+    def root_element_type
+      :repository
+    end
 
-            Reality::Logging.set_levels(verbose? ? ::Logger::DEBUG : ::Logger::WARN,
-                                        Resgen::Logger,
-                                        Reality::Generators::Logger,
-                                        Reality::Facets::Logger) do
-              Resgen.info "Generator started: Generating #{self.generator_keys.inspect}"
-              Resgen::Generator.generator.generate(:repository,
-                                                   repository,
-                                                   self.target_dir,
-                                                   @templates,
-                                                   self.filter)
-            end
-          rescue Reality::Generators::GeneratorError => e
-            puts e.message
-            if e.cause
-              puts e.cause.class.name.to_s
-              puts e.cause.backtrace.join("\n")
-            end
-            raise e.message
-          end
-        end
-        @task_name = t.name
-        Resgen::TaskRegistry.get_aggregate_task(self.namespace_key).enhance([t.name])
-      end
+    def log_container
+      Resgen
     end
   end
 
-  class LoadResourceDescriptor
-    attr_accessor :description
-    attr_accessor :namespace_key
-    attr_writer :verbose
+  class LoadResourceDescriptor < Reality::Generators::Rake::BaseLoadDescriptor
+    protected
 
-    attr_reader :filename
-
-    def initialize(filename)
-      @filename = filename
-      @namespace_key = :resgen
-      yield self if block_given?
-      define
+    def default_namespace_key
+      :resgen
     end
 
-    private
-
-    def verbose?
-      !!@verbose
+    def log_container
+      Resgen
     end
 
-    def define
-      namespace self.namespace_key do
-        task :preload
-
-        task :postload
-
-        desc self.description
-        task :load => [:preload, self.filename] do
-          begin
-            Resgen.current_filename = self.filename
-            Reality::Logging.set_levels(verbose? ? ::Logger::DEBUG : ::Logger::WARN,
-                                        Resgen::Logger,
-                                        Reality::Generators::Logger,
-                                        Reality::Facets::Logger) do
-
-              require self.filename
-            end
-          rescue Exception => e
-            print "An error occurred loading repository\n"
-            puts $!
-            puts $@
-            raise e
-          ensure
-            Resgen.current_filename = nil
-          end
-          task("#{self.namespace_key}:postload").invoke
-        end
-        Resgen::TaskRegistry.get_aggregate_task(self.namespace_key)
-      end
+    def pre_load
+      Resgen.current_filename = self.filename
     end
-  end
 
-  class TaskRegistry
-    class << self
-      def get_aggregate_task(namespace)
-        all_task = namespace_tasks[namespace.to_s]
-        unless all_task
-          desc "Generate all #{namespace} artifacts"
-          all_task = task('all')
-          namespace_tasks[namespace.to_s] = all_task
-        end
-        all_task
-      end
-
-      private
-
-      def namespace_tasks
-        @namespace_tasks ||= {}
-      end
+    def post_load
+      Resgen.current_filename = nil
     end
   end
 end
--- resgen.gemspec
+++ resgen.gemspec
@@ -25,8 +25,8 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'reality-core', '>= 1.6.0'
   s.add_dependency 'reality-facets', '>= 1.7.0'
-  s.add_dependency 'reality-generators', '>= 1.6.0'
-  s.add_dependency 'reality-naming', '>= 1.8.0'
+  s.add_dependency 'reality-generators', '>= 1.7.0'
+  s.add_dependency 'reality-naming', '>= 1.9.0'
   s.add_dependency 'reality-model', '>= 1.1.0'
   s.add_dependency 'reality-orderedhash', '>= 1.0.0'
   s.add_dependency 'sass', '>= 3.4.22'
