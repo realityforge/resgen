7fc1c57c3ffe47798612040bffdc9ad66e62d705
Peter Donald <peter@realityforge.org>
2017-05-08 07:17:26 +1000

    Move to reality-generators 1.8.0

--- lib/resgen/rake_tasks.rb
+++ lib/resgen/rake_tasks.rb
@@ -16,97 +16,77 @@ module Resgen #nodoc
   class Build #nodoc
     DEFAULT_RESOURCES_FILENAME = 'resources.rb'
 
-    def self.define_load_task(filename = nil, &block)
-      base_directory = File.dirname(Buildr.application.buildfile.to_s)
-      candidate_file = File.expand_path("#{base_directory}/#{DEFAULT_RESOURCES_FILENAME}")
-      if filename.nil?
-        filename = candidate_file
-      elsif File.expand_path(filename) == candidate_file
-        Resgen.warn("Resgen::Build.define_load_task() passed parameter '#{filename}' which is the same value as the default parameter. This parameter can be removed.")
-      end
-      Resgen::LoadResourceDescriptor.new(File.expand_path(filename), &block)
-    end
+    class << self
+      include Reality::Generators::Rake::BuildTasksMixin
 
-    def self.define_generate_task(generator_keys, options = {}, &block)
-      repository_key = options[:repository_key]
-      target_dir = options[:target_dir]
-      buildr_project = options[:buildr_project]
-
-      if buildr_project.nil? && Buildr.application.current_scope.size > 0
-        buildr_project = Buildr.project(Buildr.application.current_scope.join(':')) rescue nil
+      def default_descriptor_filename
+        DEFAULT_RESOURCES_FILENAME
       end
 
-      build_key = options[:key] || (buildr_project.nil? ? :default : buildr_project.name.split(':').last)
-
-      if target_dir
-        base_directory = File.dirname(Buildr.application.buildfile.to_s)
-        target_dir = File.expand_path(target_dir, base_directory)
+      def generated_type_path_prefix
+        :resgen
       end
 
-      if target_dir.nil? && !buildr_project.nil?
-        target_dir = buildr_project._(:target, :generated, 'resgen', build_key)
-      elsif !target_dir.nil? && !buildr_project.nil?
-        Resgen.warn('Resgen::Build.define_generate_task specifies a target directory parameter but it can be be derived from the context. The parameter should be removed.')
+      def root_element_type
+        :repository
       end
 
-      if target_dir.nil?
-        Resgen.error('Resgen::Build.define_generate_task should specify a target directory as it can not be derived from the context.')
+      def log_container
+        Resgen
+      end
+    end
+
+    class GenerateTask < Reality::Generators::Rake::BaseGenerateTask
+      def initialize(repository_key, key, generator_keys, target_dir, buildr_project = nil)
+        super(repository_key, key, generator_keys, target_dir, buildr_project)
       end
 
-      Resgen::GenerateTask.new(repository_key, build_key, generator_keys, target_dir, buildr_project, &block)
-    end
-  end
+      protected
 
-  class GenerateTask < Reality::Generators::Rake::BaseGenerateTask
-    def initialize(repository_key, key, generator_keys, target_dir, buildr_project = nil)
-      super(repository_key, key, generator_keys, target_dir, buildr_project)
-    end
+      def default_namespace_key
+        :resgen
+      end
 
-    protected
+      def generator_container
+        Resgen::Generator
+      end
 
-    def default_namespace_key
-      :resgen
-    end
+      def instance_container
+        Resgen
+      end
 
-    def generator_container
-      Resgen::Generator
-    end
+      def validate_root_element(element)
+        element.send(:extension_point, :scan_if_required)
+        element.send(:extension_point, :validate)
+      end
 
-    def instance_container
-      Resgen
-    end
+      def root_element_type
+        :repository
+      end
 
-    def validate_root_element(element)
-      element.send(:extension_point, :scan_if_required)
-      element.send(:extension_point, :validate)
-    end
-
-    def root_element_type
-      :repository
+      def log_container
+        Resgen
+      end
     end
 
-    def log_container
-      Resgen
-    end
-  end
+    class LoadDescriptor < Reality::Generators::Rake::BaseLoadDescriptor
+      protected
 
-  class LoadResourceDescriptor < Reality::Generators::Rake::BaseLoadDescriptor
-    protected
+      def default_namespace_key
+        :resgen
+      end
 
-    def default_namespace_key
-      :resgen
-    end
+      def log_container
+        Resgen
+      end
 
-    def log_container
-      Resgen
-    end
+      def pre_load
+        Resgen.current_filename = self.filename
+      end
 
-    def pre_load
-      Resgen.current_filename = self.filename
-    end
-
-    def post_load
-      Resgen.current_filename = nil
+      def post_load
+        Resgen.current_filename = nil
+      end
     end
   end
 end
