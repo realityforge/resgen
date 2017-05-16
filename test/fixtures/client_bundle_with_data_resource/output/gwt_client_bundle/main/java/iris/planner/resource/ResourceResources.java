dad2d4f62e16671c4305bbcf60df1322416a9c4c
Peter Donald <peter@realityforge.org>
2017-05-16 21:35:31 +1000

    Keep expanding the tests

---
+++ test/fixtures/client_bundle_with_data_resource/output/gwt_client_bundle/main/java/iris/planner/resource/ResourceResources.java
@@ -0,0 +1,28 @@
+/* DO NOT EDIT: File is auto-generated */
+package iris.planner.resource;
+
+@javax.annotation.Generated( "Resgen" )
+public interface ResourceResources
+  extends com.google.gwt.resources.client.ClientBundle
+{
+  ResourceResources INSTANCE = com.google.gwt.core.shared.GWT.create( ResourceResources.class );
+
+  interface Style
+    extends com.google.gwt.resources.client.CssResource
+  {
+    @ClassName( "combo" )
+    String combo();
+  }
+
+  @Source( "drop_down_arrow.png" )
+  com.google.gwt.resources.client.DataResource drop_down_arrow();
+
+  @Source( "export.gif" )
+  com.google.gwt.resources.client.ImageResource export();
+
+  @Source( "scheduled_shift.png" )
+  com.google.gwt.resources.client.ImageResource scheduled_shift();
+
+  @Source( "style.gss" )
+  Style style();
+}
---
+++ test/fixtures/client_bundle_with_images/output/gwt_client_bundle/main/java/iris/planner/resource/ResourceResources.java
@@ -0,0 +1,30 @@
+/* DO NOT EDIT: File is auto-generated */
+package iris.planner.resource;
+
+@javax.annotation.Generated( "Resgen" )
+public interface ResourceResources
+  extends com.google.gwt.resources.client.ClientBundle
+{
+  ResourceResources INSTANCE = com.google.gwt.core.shared.GWT.create( ResourceResources.class );
+
+  interface Style
+    extends com.google.gwt.resources.client.CssResource
+  {
+    @ClassName( "incomplete" )
+    String incomplete();
+    @ClassName( "requirementNotMet" )
+    String requirementNotMet();
+  }
+
+  @Source( "export.gif" )
+  com.google.gwt.resources.client.ImageResource export();
+
+  @Source( "published_shift.png" )
+  com.google.gwt.resources.client.ImageResource published_shift();
+
+  @Source( "scheduled_shift.png" )
+  com.google.gwt.resources.client.ImageResource scheduled_shift();
+
+  @Source( "style.gss" )
+  Style style();
+}
