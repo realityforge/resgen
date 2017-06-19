fdc14a76c5e902e3077f3da8247ad3123b3f90b4
Peter Donald <peter@realityforge.org>
2017-06-19 15:25:00 +1000

    Add test for font resources

---
+++ test/fixtures/client_bundle_with_font_resource/output/gwt_client_bundle/main/java/iris/planner/resource/ResourceResources.java
@@ -0,0 +1,26 @@
+/* DO NOT EDIT: File is auto-generated */
+package iris.planner.resource;
+
+@javax.annotation.Generated( "Resgen" )
+public interface ResourceResources
+  extends com.google.gwt.resources.client.ClientBundle
+{
+  ResourceResources INSTANCE = com.google.gwt.core.shared.GWT.create( ResourceResources.class );
+
+  interface Style
+    extends com.google.gwt.resources.client.CssResource
+  {
+    @ClassName( "glyphicon" )
+    String glyphicon();
+    @ClassName( "glyphicon-plus" )
+    String glyphiconPlus();
+  }
+
+  @com.google.gwt.resources.client.DataResource.DoNotEmbed
+  @com.google.gwt.resources.client.DataResource.MimeType( "application/x-font-woff" )
+  @Source( "glyphiconsHalflingsRegular.woff" )
+  com.google.gwt.resources.client.DataResource glyphiconsHalflingsRegular();
+
+  @Source( "style.gss" )
+  Style style();
+}
