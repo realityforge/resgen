3085e2601b758ea8305f99ba8e5825f4231713c9
Peter Donald <peter@realityforge.org>
2019-12-03 09:56:17 +1100

    Suppress some rawtypes warnings

--- test/fixtures/uibinder_file_with_mvp_enabled/output/gwt_abstract_uibinder_component/main/java/iris/planner/resource/AbstractSimpleShiftUI.java
+++ test/fixtures/uibinder_file_with_mvp_enabled/output/gwt_abstract_uibinder_component/main/java/iris/planner/resource/AbstractSimpleShiftUI.java
@@ -2,7 +2,7 @@
 package iris.planner.resource;
 
 @javax.annotation.Generated( "Resgen" )
-@java.lang.SuppressWarnings( { "PMD.FieldDeclarationsShouldBeAtStartOfClass", "PMD.ConstructorCallsOverridableMethod" } )
+@java.lang.SuppressWarnings( { "PMD.FieldDeclarationsShouldBeAtStartOfClass", "PMD.ConstructorCallsOverridableMethod", "rawtypes" } )
 public abstract class AbstractSimpleShiftUI
   implements com.google.gwt.user.client.ui.IsWidget
 {
